f0fda8ae057847f197cdc8534e25e727076c9298
Malik AlZubaidi <malikzu.sg@gmail.com>
2020-09-24 11:46:53 +0400

    feat: Using POST request

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## HTML to PDF converter
 This app is a simple example to use https://github.com/marcbachmann/node-html-pdf 
 
-### Setup
+## Setup
 First you have to install the packages using:
 
 ```
@@ -13,10 +13,31 @@ Then simply
 node .
 ```
 
-### Usage
+## Usage
 
+### Use it with ```GET``` 
 You have to supply two queries, `name` and `html` for example:
 
 ```
 localhost:3000/?name=mypdf&html=<h1>Hello World!</h1>
+``` 
+
+### Use it with ```POST``` 
+You have to supply in the body, `name` and `html` for example:
+
+```
+POST /post HTTP/1.1
+Host: localhost:3000
+Content-Type: application/x-www-form-urlencoded
+
+html=<div>Hello World</div>&name=mypdf
+```
+
+with `cURL`:
+
+```
+curl --location --request POST 'localhost:3000/post' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'html=<div>Hello World</div>' \
+--data-urlencode 'name=mypdf'
 ``` 
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const fs = require('fs');
 const pdf = require('html-pdf');
+const bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-
-let getReq = app.get("/", async(req,res) => {
+let getReq = app.get("/get", async(req,res) => {
     const html= req.query.html;
     const name= req.query.name;
 
@@ -20,6 +23,25 @@ let getReq = app.get("/", async(req,res) => {
               }
         });
     });
+});
+
+
+
+let postReq = app.post("/post", urlencodedParser, async(req,res) => {
+  const name= req.body.name;
+  const html= req.body.html;
+
+  pdf.create(html).toFile(`./${name}-${new Date().getUTCMilliseconds()}-${(Math.random()*10000).toFixed(0)}.pdf`, function(err, final){
+      console.log(final.filename);
+      res.download(final.filename, ()=> {
+          try {
+              fs.unlinkSync(final.filename)
+              //file removed
+            } catch(err) {
+              console.error(err)
+            }
+      });
+  });
 });
 
 app.listen(process.env.PORT || 3000, '0.0.0.0');
